5d10f6f6b263b703cf9e08f74f9fb2f4f3470a16
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-12 08:55:11 -0400

    updating trips controller

--- controllers/api/trips.js
+++ controllers/api/trips.js
@@ -18,3 +18,22 @@ async function show(req, res) {
     const trip = await Trip.findById(req.params.id);
     res.status(200).json(trip);
 }
+
+async function create(req, res) {
+    const trip = await Trip.create(req.body);
+    // const updatedUser = await User.findByIdAndUpdate(req.body.user).then(function(user) {
+    //     user.trips = [...user.trips, trip];
+    //     user.save();
+    //     return user;
+    // });
+    res.status(201).json(trip);
+}
+
+async function deleteTrip(req, res) {
+    const deletedTrip = await Trip.findByIdAndRemove(req.params.id);
+    res.status(200).json(deletedTrip);
+}
+async function update(req, res) {
+    const updatedTrip = await Trip.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).json(updatedTrip);
+}
